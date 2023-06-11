3fda8ac8952a35404b23ba44696aaec0fa864928
Kali Linux <kali@ip-172-31-15-41.us-east-2.compute.internal>
2023-06-11 04:10:11 +0000

    setup workout data/ implemented mentioned function

---
+++ BotFunctions/workoutData.py
@@ -0,0 +1,22 @@
+push = { "dumbbell press": ["dumbbell press", "3x8", 25, ""],
+         "dumbbell incline press": ["dumbbell incline press", "3x8", 20, ""],
+         "dumbbell single triceps push up": ["dumbbell single triceps push up", "3x10", 10, ""],
+         "machine single triceps push down": ["machine single triceps push down", "3x15", 25, ""],
+         "machine flies": ["machine flies", "3x10", 70, ""] }
+
+pull = { "pull up": ["pull up", "3x5", "N/A", "use all the bands or you will die"],
+         "dumbbell row": ["dumbbell row", "3x8", 20, "do it for both hands"],
+         "machine row": ["machine row", "3x10", 135, ""],
+         "hammer curls": ["hammer curls", "3x10", 15, ""] }
+
+leg = { "machine squad": ["machine squad", "2x8", 15, "the weight is pounds for each side\nmake sure don't get too right"],
+        "legs front squad": ["legs front squad", "1x10", 0, "no plates"],
+        "Romanian deadlifts": ["romanian deadlifts", "3x10", 20, ""],
+        "leg extension": ["leg extension", "3x10", 50, ""],
+        "leg curl": ["leg curl", "3x10", 65, ""] }
+
+shoulder = { "overhead press": ["overhead press", "3x10", 20, ""],
+             "reverse mahcine flies": ["reverse machine flies", "5x10", 50, "do it with laterals"],
+             "laterals" : ["laterals/stand up flies", "5x10", 10, "do it with RMF"],
+             "shrugs": ["shrugs", "3x10", 15, ""] }
+
--- workoutBot.py
+++ workoutBot.py
@@ -16,5 +16,19 @@ class Bot(discord.Client):
     async def on_message(self, message):
         print(f"message found: {message.content} from {message.author} in {message.channel}")
 
+        # if the bot it mentioned for workout
+        if client.user in message.mentions:
+            if "workout".lower() in message.content or "wo".lower() in message.content:
+                print("bot is being mentioned for workout")
+
+                await message.channel.send("Time to workout! What are you doing today?\n- pull\n- push\n- leg\n- shoulder\n")
+
+            else:
+                print("bot is being mentioned but not for workout")
+
+                await message.channel.send("You should go workout, that's the only thing I can help you.")
+
+
+
 client = Bot(intents=intents)
 client.run(token)
